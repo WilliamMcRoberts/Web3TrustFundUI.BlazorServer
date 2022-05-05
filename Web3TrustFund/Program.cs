faa9d1b7f6ec6495ecd9e269e2877507ea34248d
CincyGhost <wamcroberts2@gmail.com>
2022-05-05 14:22:22 -0400

    Add project files.

---
+++ Web3TrustFund/Program.cs
@@ -0,0 +1,32 @@
+using MetaMask.Blazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+using Web3TrustFund;
+using Web3TrustFund.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.ConfigureServices();
+
+var app = builder.Build();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NjMxMzkzQDMyMzAyZTMxMmUzMGtiT3piKzNBRXQzM2RsM1VXSjFnR2t1bDh3M3dGaGZkVTRRcmthYVFxbHc9");
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Web3TrustFund/RegisterServices.cs
@@ -0,0 +1,16 @@
+﻿using MetaMask.Blazor;
+using Syncfusion.Blazor;
+
+namespace Web3TrustFund
+{
+    public static class RegisterServices
+    {
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddMetaMaskBlazor();
+        }
+    }
+}
